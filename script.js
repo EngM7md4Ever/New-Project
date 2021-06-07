30cf90c7fd807e8f865bcaceb7cfdd2b4850b66e
shit <shit@gmail.com>
2021-06-07 11:34:38 +0200

    Created the project successfully

---
+++ script.js
@@ -0,0 +1,78 @@
+// console.log('Hello world');
+
+// var name1 = 'ahmed';
+// console.log(name1);
+// document.getElementById('the').innerHTML = age;
+
+
+// var age = prompt('Please enter Your name');
+// document.getElementById('the').innerHTML = 'Welcome Back, ' + age;
+
+// let fruit = 'B,a,n,a,n,a';
+// console.log(fruits.split(','));
+// console.log(fruits.replace('Ban','Man'));
+// console.log(fruits.toUpperCase());
+// console.log(fruits.slice(0,4));
+// console.log(fruits.indexOf('nana'));
+// console.log(fruits.length);
+
+// let fruits = new Array('Banana');
+let fruits = ['banana','apple','tomato','watermelon'];
+// fruits[1] = 'Apple';
+// fruits[2] = 'kosa';
+// fruits[3] = 'losa';
+// console.log(fruits);
+// console.log('to String',fruits.toString());
+// console.log(fruits.push('Big watermelon'),fruits);
+// console.log(fruits.pop(), fruits);
+
+
+fruits2 = ['banana2','apple2','tomato2','watermelon2'];
+
+// let allfruits = fruits.concat(fruits2);
+// console.log(allfruits);
+// console.log(allfruits.slice(2,5));
+// console.log(allfruits.reverse());
+// let Numbers = [1,2,35,45,23,12,4,5,6,7,7,7,90,90,90,129,256];
+// console.log(Numbers.sort(function (a,b){return a-b}));
+
+// StudentInfo = {
+//     firstName:'ahmed',
+//     lastName:'Ali',
+//     age:20,
+//     StudentName: function(){
+//         return this.firstName + ' \n'+ this.lastName; 
+//     }
+// };
+
+// console.log(StudentInfo.StudentName())
+
+// switch('Banana'){
+//     case'banana':
+//     text = 'Yes you like Banana';
+//     case'Banana':
+//     text = 'Banana';
+//     break;
+    
+
+//     default:
+//         text = "Oh i can't know you 've choose";
+
+
+// }
+// console.log(text);
+
+// let students =  `[      
+//     {
+//         "name":"ahmed",
+//         "age":12,
+//         "height":170
+//     },
+//     {
+//         "name":"Yasser",
+//         "age":24,
+//         "height":180
+//     }
+
+// ]`
+// console.log(JSON.parse(students))
